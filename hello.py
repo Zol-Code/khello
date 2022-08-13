2b76df241d81c461e35d8889b53ce4c953f76f48
ajzolla <ajzolla@yahoo.com>
2022-08-13 07:41:47 -0500

    Changed version number

--- hello.py
+++ hello.py
@@ -1,20 +1,14 @@
 import sys
 
-<<<<<<< HEAD
 length = len(sys.argv)
 if length == 2:
     name = sys.argv[1]
 
     if name == "--version":
-        print("KHello version 1.0")
+        print("KHello version 1.1")
         print("All rights reverved. Zolla Software Company")
     else:
         print("Hello", name + "!")
-=======
-if name == "--version":
-    print("KHello version 1.1")
-    print("All rights reverved. Zolla Software Company")
->>>>>>> CHanged version number
 else:
     print("Enter name")
 
